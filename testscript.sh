8a32c5e373826f6479d40253eda7e21abf004650
bhussain2 <Bilal.Hussain@kpmg.co.uk>
2020-01-10 16:08:43 +0000

    test

--- testscript.sh
+++ testscript.sh
@@ -2,5 +2,5 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "This is jenkins webhook t3st"
+echo "This is jenkins webhook t43st"
 echo "this was run automagically"
